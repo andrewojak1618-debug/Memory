d38bd3e4e21a926448e66f267705ff8f2a0e11ef
andrewojak1618-debug <andrewojak1618@gmail.com>
2026-08-31 17:54:43 +0200

    first start Home page

---
+++ src/vite-env.d.ts
@@ -0,0 +1,3 @@
+// Bindet die Vite-Typdefinitionen ein, damit TypeScript unter anderem SCSS-Importe erkennt.
+//Die Typdefinition verhindert lediglich, dass TypeScript den Import als unbekannt meldet.
+/// <reference types="vite/client" />
